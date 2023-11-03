a3c604c50ad71c880fca0a12fc92809b7e6b3ae4
crypto <donischle75565@gmail.de>
2023-11-03 18:28:42 +0100

    feat(LLM model): implement basic infernece test

--- LLMs/testLLM.py
+++ LLMs/testLLM.py
@@ -1,13 +1,13 @@
 from typing import List
 from ..models.LLM import InferenceLLM
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
-class DistillBertLocal(InferenceLLM):
+class Distillgpt2Local(InferenceLLM):
 
     def __init__(self, checkpoint: str):
         self.tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-        self.model = AutoModelForSequenceClassification.from_pretrained(checkpoint)  
+        self.model = AutoModelForCausalLM.from_pretrained(checkpoint)  
     
     def tokenize(self, sequence: str, sequences: List[str] = None) -> torch.Tensor:
         if not isinstance(sequence, str):
@@ -16,7 +16,7 @@ class DistillBertLocal(InferenceLLM):
         return self.tokenizer(sequence, return_tensors="pt")
     
     def invoke(self, tokens):
-        raise not NotImplementedError()
+        return self.model.generate(**tokens)
 
     def batch(self):
         raise not NotImplementedError()
--- tests/inference/test_tokenization.py
+++ tests/inference/test_tokenization.py
@@ -1,15 +1,26 @@
 import torch
+from typing import List
 import pytest
-from ...LLMs.testLLM import DistillBertLocal
+from ...LLMs.testLLM import Distillgpt2Local
 
 @pytest.mark.parametrize("sequence, expected_result", [
     ("Hello, my dog is cute", True),
     ])
 
 def test_tokenize(sequence, expected_result):
-    model = DistillBertLocal("distilbert-base-uncased-finetuned-sst-2-english")
+    model = Distillgpt2Local("distilgpt2")
     tokens =  model.tokenize(sequence)
     assert (tokens is not None) == expected_result
 
+def test_tokenize_exception():
+    model = Distillgpt2Local("distilgpt2")
+    with pytest.raises(TypeError):
+        model.tokenize(1)
+
 def test_invoke():
-    pass
+    model = Distillgpt2Local("distilgpt2")
+    tokens = model.tokenize("Hello, my dog is cute")
+    result = model.invoke(tokens)
+    print("result: ", model.tokenizer.batch_decode(result, skip_special_tokens=True))
+    assert result is not None
+    assert isinstance(model.tokenizer.batch_decode(result, skip_special_tokens=True)[0], str)
