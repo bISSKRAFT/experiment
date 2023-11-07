0ee4edaa231537607c948924d2fe8e265a0b5bbb
crypto <donischle75565@gmail.de>
2023-11-07 17:21:10 +0100

    fix(BaseLLM): types and parameters

--- src/models/llms/base.py
+++ src/models/llms/base.py
@@ -2,13 +2,14 @@ from abc import ABC, abstractmethod
 from typing import List
 from ..output import GenerationResult
 
+
 class BaseLLM(ABC):
     """A class to represent a language model"""
 
     @abstractmethod
     def _generate(
-        self,
-        prompts: List[str],
+            self,
+            prompts: List[str],
     ) -> GenerationResult:
         """Run the LLM on the given input"""
 
@@ -17,28 +18,28 @@ class BaseLLM(ABC):
             prompt: List[str],
     ) -> GenerationResult:
         """Generate a prompt from the given input"""
-        return self._generate([prompt])
-    
+        return self._generate(prompt)
+
     def invoke(self,
                prompt: str,
-    ) -> str:
+               ) -> str:
         """Generate a prompt from the given input"""
-        return self.generate_prompt(prompt).generations[0]
+        return self.generate_prompt([prompt]).generations[0]
 
 
-class InferenceLLM(BaseLLM,ABC):
-    
+class InferenceLLM(BaseLLM, ABC):
+
     @abstractmethod
     def _call(
-        self,
-        prompt: str
+            self,
+            prompt: str
     ) -> str:
         """Run the LLM on the given input"""
 
     def _generate(
-        self,
-        prompts: List[str],
-    ) -> List[str]:
+            self,
+            prompts: List[str],
+    ) -> GenerationResult:
         """Run the LLM on the given input"""
         generations = [self._call(prompt) for prompt in prompts]
         return GenerationResult(generations=generations)
