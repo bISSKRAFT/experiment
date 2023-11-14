ef58034adfd97ca98aa67065ad2ec6736f30000e
crypto <donischle75565@gmail.de>
2023-11-14 11:12:24 +0100

    fix: PEP formatting in various files

--- src/models/output.py
+++ src/models/output.py
@@ -1,11 +1,14 @@
 from typing import List
 from pydantic import BaseModel
 
+
+
+
 class GenerationResult(BaseModel):
     """A class to represent the result of a generation"""
 
     generations: List[str]
 
-    def __eq__(self, __value: object) -> bool:
+    def __eq__(self, compare: object) -> bool:
         """Check if two GenerationResult objects are equal"""
-        return self.generations == __value.generations
+        return self.generations == compare.generations
