99dd8fbac65b223a9768b465601557b6bf996edb
bISSKRAFT <102585556+bISSKRAFT@users.noreply.github.com>
2024-05-16 14:59:57 +0200

    Update base.py
    chore: remove unused code

--- src/models/profiler/base.py
+++ src/models/profiler/base.py
@@ -1,9 +1,11 @@
+import time
+from typing import Optional
 from abc import ABC, abstractmethod
 from multiprocessing.synchronize import Event
 from multiprocessing import Process, Value
 from multiprocessing.sharedctypes import Synchronized
-import time
-from typing import Optional
+
+
 
 class BaseProfiler(ABC):
 
@@ -18,27 +20,3 @@ class BaseProfiler(ABC):
     def stop(self) -> None:
         pass
 
-# class BaseAsyncProfiler(BaseProfiler,ABC):
-
-#     _time: Synchronized
-#     _process: Optional[Process]
-#     _stop_event: Event
-
-#     @abstractmethod
-#     def create_process(self, 
-#                        stop_event: Event
-#                        ) -> Process:
-#         pass
-
-#     def start(self) -> None:
-#         self._time = Value("d", time.perf_counter())
-#         if self._stop_event is None:
-#             raise ValueError("stop_event must be set before starting process")
-#         self._process = self.create_process(self._stop_event)
-#         self._process.start()
-
-#     def stop(self) -> None:
-#         self._stop_event.set()
-#         assert self._process is not None
-#         self._process.join()
-#         self._time = Value("d", time.perf_counter() - self._time.value)
